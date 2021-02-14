d34ee221926e48f8ccde0da424f99361e57c3ab1
Nawfee <44086657+nawfee@users.noreply.github.com>
2021-02-14 20:20:15 +0000

    added project of transport-pois

---
+++ analysis/transport_pois_analysis.R
@@ -0,0 +1,12 @@
+# Heading -----------------------------------------------------------------
+# Transport-related POI analysis
+#Author: Shahreen
+#Date: 15/01/2021
+
+# Clear environment-------------------------------------------------------
+rm(list = ls())
+
+#Folder setup---------------------------------------------------------
+data_folder = "C:/PhD/Transport-related-POIs/"
+
+# Load data-----------------------------------------------------
